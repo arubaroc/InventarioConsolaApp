9f877534e6bf38784bdae04d45c257e34c8799e8
Alvaro Calderón <alvarocalderonknl@gmail.com>
2026-06-01 22:57:29 -0600

    000 Primer commit, se agregó la estructura básica de proyecto .net

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estado: Proyecto inicializado");
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine("Configuracion .csproject: Define el proyecto y sus dependencias.");
+Console.WriteLine("Carpeta src/ creada para organizar el código fuente.");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Proximos pasos: Agregar argumentos CLI yy configuración del repositorio en GitHub");
